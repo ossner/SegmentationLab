e5b250eaf5406c8b2fd5fd66da82550238ff761f
Sebastian Oßner <github@sebastianoner.com>
2026-03-24 12:31:14 +0100

    Fix constant error in modal style

--- src/InfoModal.tsx
+++ src/InfoModal.tsx
@@ -36,7 +36,7 @@ export const InfoModal: React.FC<InfoModalProps> = ({ isOpen, onClose, title, ch
             padding: 0,
             boxShadow: '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)'
         },
-    };
+    } as const;
 
     return (
         <Modal
